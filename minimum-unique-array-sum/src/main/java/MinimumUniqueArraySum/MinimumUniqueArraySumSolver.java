17c42e8cc0b61ffc8d2335933f7a4419d7a665d4
sebastian-blaszczak <sebastian.blaszczak.1994@gmail.com>
2019-03-16 13:40:05 +0100

    Adding new programming task

---
+++ minimum-unique-array-sum/src/main/java/MinimumUniqueArraySum/MinimumUniqueArraySumSolver.java
@@ -0,0 +1,13 @@
+package MinimumUniqueArraySum;
+
+/**
+ * Hello world!
+ *
+ */
+public class MinimumUniqueArraySumSolver
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ minimum-unique-array-sum/src/test/java/MinimumUniqueArraySum/MinimumUniqueArraySumSolverTest.java
@@ -0,0 +1,20 @@
+package MinimumUniqueArraySum;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple MinimumUniqueArraySumSolver.
+ */
+public class MinimumUniqueArraySumSolverTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
