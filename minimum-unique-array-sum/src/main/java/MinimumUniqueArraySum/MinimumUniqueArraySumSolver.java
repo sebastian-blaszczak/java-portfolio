5d20e95a61f063da6aa1b1c2b03c1b0230b16b0c
sebastian-blaszczak <sebastian.blaszczak.1994@gmail.com>
2019-03-17 08:19:05 +0100

    Update minimum unique array sum solver and test

--- minimum-unique-array-sum/src/main/java/MinimumUniqueArraySum/MinimumUniqueArraySumSolver.java
+++ minimum-unique-array-sum/src/main/java/MinimumUniqueArraySum/MinimumUniqueArraySumSolver.java
@@ -3,14 +3,23 @@ package MinimumUniqueArraySum;
 
 import java.util.Arrays;
 
-public class MinimumUniqueArraySumSolver {
+class MinimumUniqueArraySumSolver {
 
-    public static int solve(int[] integers) {
+    /**
+     * Minimum unique array sum is an array that sum only unique numbers if there are two the same numbers it
+     * will add 1 to one of number.
+     * @param integers array set to sum
+     * @return sum of unique array elements
+     */
+    static int solve(int[] integers) {
+        if (integers.length == 0) {
+            throw new IllegalArgumentException("Array cannot be empty!");
+        }
         Arrays.sort(integers);
 
-        int previous = 0;
+        int previous = Integer.MIN_VALUE;
         for (int i = 0; i < integers.length; i++) {
-            if (previous != 0 && previous >= integers[i]) {
+            if (previous >= integers[i]) {
                 integers[i] = previous + 1;
             }
             previous = integers[i];
--- minimum-unique-array-sum/src/test/java/MinimumUniqueArraySum/MinimumUniqueArraySumSolverTest.java
+++ minimum-unique-array-sum/src/test/java/MinimumUniqueArraySum/MinimumUniqueArraySumSolverTest.java
@@ -5,20 +5,47 @@ import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Unit test for Minimum Array Sum Solver
+ */
+class MinimumUniqueArraySumSolverTest {
 
-public class MinimumUniqueArraySumSolverTest {
-
-    int[] testSet = new int[]{1, 2, 3, 3, 4, 5};
-    int [] testSet2 = new int [] {1,1,1,1,1};
+    private int[] testSet = new int[]{1, 2, 3, 3, 4, 5};
+    private int[] testSetAllEquals = new int[]{0, 0, 0, 0, 0};
+    private int[] testSetAllNegative = new int[]{-1, -10, -4, -4, -4};
+    private int[] testSetNegativeAndPositive = new int[]{1, 1, -1, -3, -3};
+    private int[] testSetThatThrowsException = new int[]{};
 
     @Before
-    public void setUpTestSet() {
+    void settingUpTestSets() {
         testSet = new int[]{1, 2, 3, 3, 4, 5};
+        testSetAllEquals = new int[]{0, 0, 0, 0, 0};
+        testSetAllNegative = new int[]{-1, -10, -4, -4, -4};
+        testSetNegativeAndPositive = new int[]{1, 1, -1, -3, -3};
     }
 
     @Test
-    public void minimumUniqueArraySolverTest() {
+    void minimumUniqueArraySolverTest() {
         Assertions.assertThat(MinimumUniqueArraySumSolver.solve(testSet)).isEqualTo(21);
-        Assertions.assertThat(MinimumUniqueArraySumSolver.solve(testSet2)).isEqualTo(15);
+    }
+
+    @Test
+    void minimumUniqueArraySolverAllEqualsTest() {
+        Assertions.assertThat(MinimumUniqueArraySumSolver.solve(testSetAllEquals)).isEqualTo(10);
+    }
+
+    @Test
+    void minimUniqueArraySolverAllNegative() {
+        Assertions.assertThat(MinimumUniqueArraySumSolver.solve(testSetAllNegative)).isEqualTo(-20);
+    }
+
+    @Test
+    void minimumUniqueArraySolverNegativeAndPositive() {
+        Assertions.assertThat(MinimumUniqueArraySumSolver.solve(testSetNegativeAndPositive)).isEqualTo(-3);
+    }
+
+    @Test
+    void minimumUniqueArraySolverThatThrowsException() {
+        Assertions.assertThat(MinimumUniqueArraySumSolver.solve(testSetThatThrowsException)).isEqualTo(-3);
     }
 }
